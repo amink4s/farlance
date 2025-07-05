b8241d77a6f67f38c06fdacc09f6e91b41fbd6a5
mika <kasaie.amin@gmail.com>
2025-07-05 17:27:27 +0330

    fix: Added unoptimized prop to PFP Image component to bypass image optimizer issues

--- app/page.tsx
+++ app/page.tsx
@@ -119,6 +119,7 @@ export default function App() {
                       width={64}
                       height={64}
                       className="rounded-full"
+                      unoptimized={true}
                     />
                   )}
                   <div>
